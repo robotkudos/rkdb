fbf23d4a21350ecc6856ae82e30e1a3879746791
Nima Hejazi <nima@robotkudos.com>
2020-07-30 14:00:17 -0700

    Add PHPDoc block

--- src/Options.php
+++ src/Options.php
@@ -15,10 +15,29 @@ class Options
         $this->options = Option::all();
     }
 
+    /**
+     * Get the value from DB
+     * 
+     * @param string $key The key to get the value of
+     * @param string $default (optional) The default value if key not found
+     * 
+     * @return string The value of the key found
+     */
     public function get($key, $default = null) {
         return $this->getValueByKey($this->options, $key, $default);
     }
 
+     /**
+     * Add or update a value to the Options table.
+     * 
+     * @param string $key The key to be inserted or updated
+     * @param string $value The value for update
+     * @param string $group_key The group key name for this option, if doesn't exist, will be created.
+     * @param string $title (optional) The title for the option key, if it needs to be created, will be used.
+     * @param string $group_title (optional) The title for option group key, if has to be created
+     * @param string $type (optional) The type of data for this option
+     * 
+     */
     public function set($key, $value, $group_key, $title = null, $group_title = null, $type = 'text') {
         if (!OptionGroup::where('key', $group_key)->exists()) {
             if ($group_title === null) $group_title = $group_key;
